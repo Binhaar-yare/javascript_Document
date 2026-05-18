265fb3259c5808f3c5cb0e41ec13d3c0c093eaa6
Binhaar-yare <binhaar.bb@gmail.com>
2026-05-18 10:35:15 +0300

    this javascript

---
+++ style.js
@@ -0,0 +1,26 @@
+//this is first line of javacript
+// console.log('this is the first line of javacript');
+// console.log('hello world')
+
+//this is variable declaration
+
+// let name = 'Bashir'
+// let age = 35
+// let isMarried = true
+// let city = 'Mogadishu'
+
+// console.log(name)
+// console.log(age)
+// console.log(isMarried)
+// console.log(city)
+
+//this is calculator
+
+let num1 = 10
+let num2 = 20
+let sum = num1 + num2
+console.log('the sum of '+num1 +' and '+num2+' is: '+sum)
+let sub = num1 - num2
+console.log('the sub of ' + num1+ ' and ' + num2 + ' is:' + sub)
+let  mul = num1 * num2
+console.log ('the mul of ' + mul)

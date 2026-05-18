af569e91af0c1c9e309d1b168e0c905229c3ac58
Binhaar-yare <binhaar.bb@gmail.com>
2026-05-18 11:37:24 +0300

    ilaa nested if

--- style.js
+++ style.js
@@ -16,11 +16,92 @@
 
 //this is calculator
 
-let num1 = 10
-let num2 = 20
-let sum = num1 + num2
-console.log('the sum of '+num1 +' and '+num2+' is: '+sum)
-let sub = num1 - num2
-console.log('the sub of ' + num1+ ' and ' + num2 + ' is:' + sub)
-let  mul = num1 * num2
-console.log ('the mul of ' + mul)
+// let num1 = 10
+// let num2 = 20
+// let sum = num1 + num2
+// console.log('the sum of '+num1 +' and '+num2+' is: '+sum)
+// let sub = num1 - num2
+// console.log('the sub of ' + num1+ ' and ' + num2 + ' is:' + sub)
+// let  mul = num1 * num2
+// console.log ('the mul of ' + mul)
+
+// // this is alert
+// let name = prompt ('what is your name sir?')
+// alert('welcome to my website' + name)
+
+// Conditional statement
+// let age = 17
+// if (age >= 18){
+//     console.log('you are eligible to vote')
+// } else{
+//     console.log ('you are not eligible to vote')
+// }
+// let password = 1234
+// if (password == 123){
+//     console.log('login successful')
+// } else {
+//     console.log('login failed')
+// }
+// let score =70
+// if(score >= 90){
+//     console.log('A')
+// } else if(score >= 80){
+//     console.log('B')
+// }else if(score >= 70){
+//     console.log('C')
+// }else {
+//     console.log('F')
+// }
+
+// let num = 60
+// let isMarried = true
+// if(num >= 10 && isMarried == true){
+//     console.log('allowed')
+// }
+//  let isAdmin = false
+//  let isEdit = true
+//  if(isAdmin || isEdit){
+//     console.log('allowd access')
+//  }
+
+// let isLoginIn = true
+// if(isLoginIn){
+//     console.log('please login')
+// }
+
+// let age = prompt('Enter your age')
+// if(age >= 18){
+//     alert('you are eligible to vote')
+// }else{
+//     alert('you are not eligible to vote')
+// }
+
+// let num = prompt('enter a number')
+// if(num % 2 ==0){
+//     alert('even')
+// }else{
+//     alert('odd')
+// }
+
+// mini project
+// let email = prompt('Enter your Email')
+// let password = prompt ('Enter your Password')
+// if(email == 'admin@gmail.com' &&password == 'admin123'){
+//     alert('login successful')
+// }else{
+//     alert('login failed')
+// }
+
+// Nested if statement
+// let age = 20
+// let isMan = true
+// if(age >= 20){
+//     if(isMan == false){
+//         console.log('welcome')
+//     }else{
+//         console.log('buy ticket')
+//     }
+  
+// }  else{
+//         console.log('you are not allowed')
+//     }

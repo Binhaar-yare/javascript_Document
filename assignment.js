c5bd3a1b2617cb165ecf83ae6ac30757e26e45a0
Binhaar-yare <binhaar.bb@gmail.com>
2026-05-19 09:10:35 +0300

    waa assignment javascript ah

---
+++ assignment.js
@@ -0,0 +1,42 @@
+//Exercise 1
+// let age = prompt('Enter your age')
+// if(age >=18){
+//     alert('you are adult')
+    
+// }else{
+//     alert('you are not adult')
+// }
+
+//exercise 2
+// let score = prompt('Enter your Score')
+// if(score >= 90){
+//     alert('A')
+// }else if(score >= 80){
+//     alert('B')
+// }else if(score >= 70){
+//     alert('C')
+// }else if(score >= 60){
+//     alert('D')
+// }else{
+//     alert('Fail')
+// }
+// Exercise 3
+
+// let email = prompt('Enter your email')
+// let password = prompt('Enter your password')
+// if (email == 'admin@gmail.com' && password == 'admin123'){
+//     alert('login successful')
+// }else{
+//     alert ('login failed')
+// }
+
+// Exercise 4
+// let num = prompt('Enter a number')
+// if(num > 0){
+//     alert('positive')
+// }else if(num < 0 ){
+//     alert('negative')
+
+// }else{
+//     alert('zero')
+// }
